bd526ac6869e82b53f580576b4d3749d62e10b6a
qiuggv <>
2019-11-01 11:40:33 +0800

    上传代码到分支[master], recordId=34

---
+++ src/main/java/com/myapp/mybatis006/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.myapp.mybatis006.mapper;
+
+import com.myapp.mybatis006.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
